25b49ef9d9e9bb6aa538f71a4d8d8c5a51cdd06f
Andy <97325945+anndy145@users.noreply.github.com>
2024-12-07 16:45:59 +0800

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ from flask import Flask, request, abort
 from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
 from linebot.models import MessageEvent, LocationMessage, TextSendMessage
-import config
 import json
 import math
 
